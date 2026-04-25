3581d7f9cbbb2aea39f02a2658bf72cb011dbd8b
samuelasta <samuela.castanom@uqvirtual.edu.co>
2026-04-25 16:32:15 -0500

    rotar matriz recursivamente

--- src/A/RecursiveRotation.java
+++ src/A/RecursiveRotation.java
@@ -1,5 +1,7 @@
 package A;
 
+import java.util.jar.JarEntry;
+
 public class RecursiveRotation {
 
     // devuelve la matriz rotada
@@ -9,32 +11,67 @@ public class RecursiveRotation {
             return matriz;
         }
 
-        String [][] nuevaMatriz = new String[sizeFilas][sizeColumnas];
+        String [][] nuevaMatriz = new String[sizeColumnas][sizeFilas];
+
+        nuevaMatriz = rotarMatriz(matriz, nuevaMatriz, sizeFilas, sizeColumnas, 0, 0, giros );
 
         if(giros > 0) {
-            matriz =  rotarMatriz(matriz, nuevaMatriz, sizeFilas, sizeColumnas );
-            girarRuleta(matriz, giros - 1, sizeFilas, sizeColumnas);
+            return girarRuleta(nuevaMatriz, giros - 1, sizeFilas, sizeColumnas);
         }
 
         else{
-            matriz =  rotarMatriz(matriz, nuevaMatriz, sizeFilas, sizeColumnas);
-            girarRuleta(matriz, giros + 1, sizeFilas, sizeColumnas);
+            return girarRuleta(nuevaMatriz, giros + 1, sizeFilas, sizeColumnas);
+        }
+    }
+
+    public static String [][] rotarMatriz(String [][] matriz, String [][] nuevaMatriz, int sizeFilas, int sizeColumnas, int i, int j, int giros) {
+
+        if (i == sizeFilas) {
+            return nuevaMatriz;
         }
 
-        return matriz;
+        if (giros > 0) {
+            nuevaMatriz[j][(sizeFilas - 1) - i] = matriz[i][j];
+        }
+        else{
+            nuevaMatriz[(sizeColumnas - 1) - j] [i] = matriz[i][j];
+        }
+
+        if(j == sizeColumnas -1){
+            return rotarMatriz(matriz, nuevaMatriz, sizeFilas, sizeColumnas,i + 1, 0, giros);
+        }
+        else return rotarMatriz(matriz, nuevaMatriz, sizeFilas, sizeColumnas, i , j + 1, giros);
     }
 
-    public static String [][] rotarMatriz(String [][] matriz, String [][] nuevaMatriz, int sizeFilas, int sizeColumnas){
+    public static void print(String [][] matriz, int i, int j){
+        if(i == matriz.length){
+            return;
+        }
+        String car = matriz [i][j];
+        System.out.print(car+ " ");
+        if(j == matriz[0].length - 1){
+            System.out.println();
+        }
 
-
-        return null;
+        if(j == matriz[0].length - 1){
+            print(matriz, i + 1, 0);
+        }
+        else{
+            print(matriz, i, j + 1);
+        }
     }
-
-
 
     public static void main(String[] args) {
 
-        rotarMatriz(null, null, 0, 0);
+        String [][] matriz = {
+                {"N", "J", "K"},
+                {"I", "Z", "Y"},
+                {"U", "E", "R"}
+        };
+
+        String [][] rotatedMatriz = girarRuleta(matriz, 2,3, 3);
+        print(rotatedMatriz, 0,0);
+
     }
 
 }
